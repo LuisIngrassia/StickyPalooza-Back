3a775893cdb21fb40a55cc75ab47a370cb516202
lui <ingrassialuispedro@gmail.com>
2024-08-13 17:47:49 -0300

    service code

--- server/src/main/java/com/g12/tpo/server/service/CategoryService.java
+++ server/src/main/java/com/g12/tpo/server/service/CategoryService.java
@@ -1,1 +1,23 @@
 package com.g12.tpo.server.service;
+
+import java.util.ArrayList;
+import java.util.Locale.Category;
+
+import com.g12.tpo.server.repository.CategoryRepository;
+
+public class CategoryService {
+    public ArrayList<Category> getCategories() {
+        CategoryRepository categoryRepository = new CategoryRepository();
+        return categoryRepository.getCategories();
+    }
+
+    public String getCategoryById(int categoryId) {
+        CategoryRepository categoryRepository = new CategoryRepository();
+        return categoryRepository.getCategoryById(categoryId);
+    }
+
+    public String createCategory(String entity) {
+        CategoryRepository categoryRepository = new CategoryRepository();
+        return categoryRepository.createCategory(entity);
+    }
+}
