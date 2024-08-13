1870e99d0d55761cb29a92a5e118b71045459cd4
lui <ingrassialuispedro@gmail.com>
2024-08-13 17:41:07 -0300

    entity code y Repository code

--- server/src/main/java/com/g12/tpo/server/entity/Category.java
+++ server/src/main/java/com/g12/tpo/server/entity/Category.java
@@ -1,1 +1,10 @@
 package com.g12.tpo.server.entity;
+
+import lombok.Data;
+
+
+@Data
+public class Category {
+    private int id;
+    private String descriprion;
+}
--- server/src/main/java/com/g12/tpo/server/repository/CategoryRepository.java
+++ server/src/main/java/com/g12/tpo/server/repository/CategoryRepository.java
@@ -1,1 +1,27 @@
 package com.g12.tpo.server.repository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale.Category;
+
+
+public class CategoryRepository {
+    public ArrayList<Category> categories = new ArrayList<Category>(
+        Arrays.asList(Category.builder().description("Series").id(1).build(),
+        Category.builder().description("Peliculas").id(2).build(),
+        Category.builder().description("Musica").id(3).build())
+        );
+
+        public ArrayList<Category> getCategories() {
+            return this.categories;
+        }
+
+        public Category getCategoryById(int categoryId) {
+            return null;
+        }
+
+        public String createCategory(String entity) {
+            return null;
+        }
+    
+}
