9e7a9b253c8b07a0a720456e406d1ea784858406
JUANITOss <0juansuazo0@gmail.com>
2024-08-13 17:49:09 -0300

    Primera parte de controller (setteo de funciones)

--- server/src/main/java/com/g12/tpo/server/controllers/CategoriesController.java
+++ server/src/main/java/com/g12/tpo/server/controllers/CategoriesController.java
@@ -1,4 +1,31 @@
 package com.g12.tpo.server.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("categories")
 public class CategoriesController {
+
+    @GetMapping
+    public String getCategories() {
+        return new String();
+    }
+    
+    @GetMapping("/{categoryId}")
+    public String getCategoryById(@PathVariable String categoryId) {
+        return new String();
+    }
+    
+    @PostMapping
+    public String createCategory(@RequestBody String entity) {
+        return entity;
+    }
+    
+
 }
