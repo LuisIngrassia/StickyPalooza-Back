0e51860a965e919adfac8f24b48d3f76975fe375
tomas <regueiratomas123@gmail.com>
2024-08-20 19:21:25 -0300

    ProductService realizado

--- server/src/main/java/com/g12/tpo/server/service/ProductService.java
+++ server/src/main/java/com/g12/tpo/server/service/ProductService.java
@@ -1,5 +1,48 @@
 package com.g12.tpo.server.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.g12.tpo.server.entity.Product;
+import com.g12.tpo.server.repository.ProductRepository;
+
+import java.util.List;
+
+@Service
 public class ProductService {
-    
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Product no encontrado"));
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    // REVISAR UPDATEPRODUCT :V
+
+    public Product updateProduct(Long id, Product productDetails) {
+        Product product = getProductById(id);
+        product.setName(productDetails.getName());
+        product.setDescription(productDetails.getDescription());
+        product.setPrice(productDetails.getPrice());
+        product.setCategory(productDetails.getCategory());
+        product.setStockQuantity(productDetails.getStockQuantity());
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        if (!productRepository.existsById(id)) {
+            throw new RuntimeException("Producto no encontrado");
+        }
+        productRepository.deleteById(id);
+    }
 }
