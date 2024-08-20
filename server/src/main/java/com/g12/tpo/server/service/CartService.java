24202d44772e510dd47301c274a9acaeabf818da
JUANITOss <0juansuazo0@gmail.com>
2024-08-20 19:09:14 -0300

    Service y Controller de Cart (Falta testeo)

--- server/src/main/java/com/g12/tpo/server/service/CartService.java
+++ server/src/main/java/com/g12/tpo/server/service/CartService.java
@@ -1,5 +1,38 @@
 package com.g12.tpo.server.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.g12.tpo.server.entity.Cart;
+import com.g12.tpo.server.repository.CartRepository;
+
+import java.util.List;
+
+@Service
 public class CartService {
-    
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    public Cart createCart(Cart cart) {
+        return cartRepository.save(cart);
+    }
+
+    public Cart getCartById(Long id) {
+        return cartRepository.findById(id).orElseThrow(() -> new RuntimeException("Cart not found"));
+    }
+
+    public List<Cart> getAllCarts() {
+        return cartRepository.findAll();
+    }
+
+    public Cart updateCart(Long id, Cart cartDetails) {
+        Cart cart = getCartById(id);
+        cart.setProducts(cartDetails.getProducts());
+        return cartRepository.save(cart);
+    }
+
+    public void deleteCart(Long id) {
+        cartRepository.deleteById(id);
+    }
 }
