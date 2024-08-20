19126b37d26288524e18a11cf20331d038b308fa
lui <ingrassialuispedro@gmail.com>
2024-08-20 19:08:22 -0300

    Order files

--- server/src/main/java/com/g12/tpo/server/controllers/OrderController.java
+++ server/src/main/java/com/g12/tpo/server/controllers/OrderController.java
@@ -1,5 +1,24 @@
 package com.g12.tpo.server.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.g12.tpo.server.service.OrderService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("orders")
 public class OrderController {
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getOrders() {
+        return ResponseEntity.ok(orderService.getOrders);
+    }
     
 }
--- server/src/main/java/com/g12/tpo/server/entity/User.java
+++ server/src/main/java/com/g12/tpo/server/entity/User.java
@@ -1,6 +1,8 @@
 package com.g12.tpo.server.entity;
 
 import java.util.List;
+
+import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
