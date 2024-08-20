ae4e30e1e6f9c4631ec050a6d5ba4b7ffc8e25e5
JUANITOss <0juansuazo0@gmail.com>
2024-08-20 18:35:40 -0300

    Order Repository y Order Entity

--- server/src/main/java/com/g12/tpo/server/entity/Order.java
+++ server/src/main/java/com/g12/tpo/server/entity/Order.java
@@ -1,6 +1,25 @@
 package com.g12.tpo.server.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Date;
+import java.util.Set;
 
+@Entity
+@Data
+@Table(name = "orders")
 public class Order {
-  
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Date orderDate;
+    private Double totalPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private Set<Product> products;
 }
