d214b13402d8b2ce92e81f01cba1a01d8fdfe36f
lui <ingrassialuispedro@gmail.com>
2024-08-20 18:37:02 -0300

    Role, user file

--- server/src/main/java/com/g12/tpo/server/entity/User.java
+++ server/src/main/java/com/g12/tpo/server/entity/User.java
@@ -1,5 +1,27 @@
 package com.g12.tpo.server.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
     
+    @Column
+    private String name;
+
+    @Column
+    private String username;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
 }
