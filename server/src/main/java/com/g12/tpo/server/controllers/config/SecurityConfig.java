dab5e170552339e13fc4f922b4c25fa8145016f9
JUANITOss <0juansuazo0@gmail.com>
2024-08-29 21:44:24 -0300

    IGNORAR COMMIT -- Comentadas funciones no usadas

--- server/src/main/java/com/g12/tpo/server/controllers/config/SecurityConfig.java
+++ server/src/main/java/com/g12/tpo/server/controllers/config/SecurityConfig.java
@@ -6,10 +6,10 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
+// import org.springframework.security.web.authentication.logout.LogoutHandler;
+// import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.g12.tpo.server.entity.Role;
 
--- server/src/main/java/com/g12/tpo/server/entity/User.java
+++ server/src/main/java/com/g12/tpo/server/entity/User.java
@@ -14,14 +14,15 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.JoinTable;
+// import jakarta.persistence.ManyToMany;
+
 
 @Data
 @Builder
