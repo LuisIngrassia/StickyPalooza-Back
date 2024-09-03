22db66a6a46ee78722d7ca3176951fb03c78835e
JUANITOss <0juansuazo0@gmail.com>
2024-09-03 19:23:22 -0300

    tan tests no son pq no testean nada pero hay tests (que no testean nada)

--- server/src/test/java/com/g12/tpo/server/integrationTests/UserControllerIntegrationTest.java
+++ server/src/test/java/com/g12/tpo/server/integrationTests/UserControllerIntegrationTest.java
@@ -8,7 +8,6 @@ import com.g12.tpo.server.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
