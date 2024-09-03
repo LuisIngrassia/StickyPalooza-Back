ba399aefd706ba15a44f37cbcdf02011a0354d38
lui <ingrassialuispedro@gmail.com>
2024-09-03 19:21:59 -0300

    bill repository

--- server/src/main/java/com/g12/tpo/server/repository/BillRepository.java
+++ server/src/main/java/com/g12/tpo/server/repository/BillRepository.java
@@ -1,12 +1,19 @@
 package com.g12.tpo.server.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.g12.tpo.server.entity.Bill;
 
-@Service
+@Repository
 public interface BillRepository extends JpaRepository<Bill, Long> {
-    
-}
 
+    @Query(value = "select b from Bill b where b.user.id = ?1")
+    List<Bill> findByUserId(Long userId);
+
+    @Query(value = "select b from Bill b where b.totalAmount > ?1")
+    List<Bill> findByTotalAmountGreaterThan(Double amount);
+}
