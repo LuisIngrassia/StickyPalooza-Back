cc1d7fceacb81a1b6b24014d610fb89f08f15daf
JUANITOss <0juansuazo0@gmail.com>
2024-10-19 19:41:08 -0300

    mejorado el apartado visual, order empezado

--- server/src/main/java/com/g12/tpo/server/controllers/config/SecurityConfig.java
+++ server/src/main/java/com/g12/tpo/server/controllers/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
                 .requestMatchers("/error/**").permitAll()
                 .requestMatchers("/categories/**").authenticated()
                 .requestMatchers("/carts/**").authenticated()
-                .requestMatchers("/products/**").authenticated()
+                .requestMatchers("/products/**").permitAll()
                 .requestMatchers("/bills/**").authenticated()
                 .requestMatchers("/orders/**").authenticated()
                 .requestMatchers("/users/**").authenticated())
--- server/src/main/java/com/g12/tpo/server/repository/OrderRepository.java
+++ server/src/main/java/com/g12/tpo/server/repository/OrderRepository.java
@@ -1,5 +1,7 @@
 package com.g12.tpo.server.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.g12.tpo.server.entity.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long>{
+    List<Order> findByUserId(Long userId);
 }
