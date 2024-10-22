0bbb11bc781451867c1a7f3bce33d5dd98a6a52d
tomas <regueiratomas123@gmail.com>
2024-10-22 00:42:30 -0300

    espero q sea el ultimo commit del back

--- server/src/main/java/com/g12/tpo/server/controllers/config/WebConfig.java
+++ server/src/main/java/com/g12/tpo/server/controllers/config/WebConfig.java
@@ -14,6 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 // .addResourceLocations("classpath:/static/uploads/");
-                .addResourceLocations("file:/home/juanchi/workspace/StickyPalooza-Back/server/src/main/resources/static/uploads");
-    }
+
+                .addResourceLocations("file:/home/tomas/StickyPalooza-Back/server/src/main/resources/static/uploads/");
+                
+                // HOLA CUANDO QUIERAS VER IMAGENES DESCOMENTA ESTO Y COMENTA LA DE ARRIBA 
+                //.addResourceLocations("file:/home/juanchi/workspace/StickyPalooza-Back/server/src/main/resources/static/uploads/");
+                
+                // PROFE SI ESTAS LEYENDO ESTO POR FAVOR PERDON YA SE QUE ESTA MAL EL MANEJO DE IMAGENES 
+                // CON UNA RUTA ASI PERO ES MOMENTANEO, VA A SER ARREGLADO SI O SI GRACIAS LE MANDO UN SALUDO!!
+            }
 }
